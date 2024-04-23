82277996d0b5c4b7622f2fb193824bc0ddaa8dd9
patricklucescu <patricklucescu@outlook.com>
2024-04-23 14:03:06 +0200

    setup config

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="codezilla",
+    name="utility_belt",
     version="0.1",
     packages=find_packages(),
     description="Basic python functions to connect to databases and other essential stuff.",
