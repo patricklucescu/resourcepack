bd8361e2b0f34ef05647295f3fb1fad7099560e5
patricklucescu <patricklucescu@outlook.com>
2024-04-23 20:42:51 +0200

    updated to a class for the database module. incorporated connection to both clickhouse and postgresql

---
+++ resourcepack/errors/base.py
@@ -0,0 +1,18 @@
+
+
+class ValidFileError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+
+
+class FileExtensionError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+
+
+class DatabaseNameError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
---
+++ tests/test_database.py
@@ -0,0 +1,10 @@
+import pytest
+import os
+
+from resourcepack.database import Database
+
+
+config_path = os.environ['CONFIG_PATH']
+
+
+
